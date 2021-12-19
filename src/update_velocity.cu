3394d3a76af3d1cea45204e0c8c2a8822ef04626
Qingyuan Qie <will.qie@mail.utoronto.ca>
2021-12-18 20:32:46 -0500

    fix typo in update_velocity

--- src/update_velocity.cu
+++ src/update_velocity.cu
@@ -14,5 +14,5 @@ __global__ void update_velocity(
 
     velocity[i].x = (new_positions[i].x - positions[i].x) / dt;
     velocity[i].y = (new_positions[i].y - positions[i].y) / dt;
-    velocity[i].z = (new_positions[i].y - positions[i].y) / dt;
+    velocity[i].z = (new_positions[i].z - positions[i].z) / dt;
 }
